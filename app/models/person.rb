d3f3ac76037000ed3152bbfa3c839507ef6f5258
priyanka1-webonise <ukirde.priyanka@webiniselab.com>
2013-01-04 21:06:38 +0530

    working on validations

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,4 +3,11 @@ class Person < ActiveRecord::Base
   attr_accessible :email, :password, :person_role
   has_one :user
   has_one :business_user
+
+  before_save { |user| user.email = email.downcase }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130104132631) do
+ActiveRecord::Schema.define(:version => 20130104144101) do
 
   create_table "business_companies", :force => true do |t|
     t.string   "business_name"
@@ -74,6 +74,8 @@ ActiveRecord::Schema.define(:version => 20130104132631) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
+
+  add_index "people", ["email"], :name => "index_people_on_email", :unique => true
 
   create_table "posts", :force => true do |t|
     t.string   "content"
