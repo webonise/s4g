f1a91fe3702f635587171588cbe5aaa078b332ec
webonise <webonise@webonise-desktop.(none)>
2013-01-07 11:50:54 +0530

    edit and update methos created

--- app/controllers/causes_controller.rb
+++ app/controllers/causes_controller.rb
@@ -1,14 +1,35 @@
 class CausesController < ApplicationController
+
   def new
     @cause=Cause.new
   end
+
   def create
+
     @cause=Cause.new(params[:@cause])
+
     if @cause.save
       flash[:success] = "Welcome to the Sample App!"
       redirect_to '/create_cause'
-else
+    else
       render 'new'
+    end
+
+  end
+
+  def edit
+    @cause=Cause.find(params[:id])
+  end
+
+  def update
+    @cause=Cause.find(params[:id])
+
+    if @cause.update_attributes(params[:cause])
+      flash[:success]="Cause Updated Successfully"
+      redirct_to @cause
+    else
+      render 'edit'
+
     end
   end
 end
