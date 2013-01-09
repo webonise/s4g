f41d14c1f368eb561b71cdfd1ba782f558fca3f7
priyanka1-webonise <ukirde.priyanka@webiniselab.com>
2013-01-09 22:21:53 +0530

    logo added to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -24,8 +24,8 @@ S4g::Application.routes.draw do
   end
 
   resources :posts, only: [:create, :destroy]
-  match '/',  to: 'users#new'
-  match '/user_signup',  to: 'users#new', :as => 'user_sign_up'
+
+  match '/user_sign_up',  to: 'users#create', :as => 'user_sign_up'
   match '/business_user_signup', to: 'business_users#new', :as => 'business_user_sign_up'
   match '/business_details', to: 'business_users#get_business_details', :as => 'business_details'
   match '/', to: 'static_pages#home'
@@ -84,7 +84,7 @@ S4g::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
 
-   root :to => 'users#new'
+   root :to => 'static_pages#home'
 
 
   # See how all your routes lay out with "rake routes"
