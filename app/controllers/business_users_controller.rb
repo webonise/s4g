0f6b6d36d686c88c104e2ac1fd42931e20c2046b
Nishant Samel <nishants@weboniselab.com>
2013-01-10 14:05:30 +0530

    Business User Activity done till posts shown.

--- app/controllers/business_users_controller.rb
+++ app/controllers/business_users_controller.rb
@@ -10,18 +10,16 @@ class BusinessUsersController < ApplicationController
 
   def new
     @business_user = BusinessUser.new
-    #logger.info("#################{@business_user.inspect}")
   end
 
   def create
     @business_user = BusinessUser.new(params[:business_user])
 
-
     if @business_user.save
-      #logger.info("#################{@business_user.inspect}")
       #sign_in @business_user
       flash[:success] = "Welcome!"
-      redirect_to get_business_detail_business_user_path(:id => @business_user.id)
+      redirect_to get_business_detail_business_user_path(@business_user)
+      #redirect_to get_business_detail_business_user_path(:id => @business_user.id)
     else
       render 'new'
     end
@@ -30,7 +28,7 @@ class BusinessUsersController < ApplicationController
   def get_cause_to_business
     @causes = Cause.all
     @business_user = BusinessUser.find(params[:id])
-    #logger.info("#################{@causes.inspect}")
+    #logger.info("###########################{@business_user.inspect}")
   end
 
   def save_business_cause
@@ -47,39 +45,29 @@ class BusinessUsersController < ApplicationController
         #TODO: redirecting to Business User Dashboard
       end
     end
-    #logger.info "#################{@business_cause.inspect}"
   end
 
   def show_post
     @business_user = BusinessUser.find(params[:id])
+    #logger.info("###########################{@business_user.inspect}"
     @business_company = @business_user.business_company
+    logger.info("###########################{@business_company.inspect}")
     @post = @business_company.posts.new
-    @posts = @business_company.posts.order(creted_at desc).paginate(page: params[:page])
+    @posts = @business_company.posts.order("created_at desc").paginate(page: params[:page])
   end
 
   def get_business_detail
-    #logger.info("#################{params[:id]}")
     @business_user = BusinessUser.find(params[:id])
-    @business_company = BusinessCompany.new()
-    @business_company.business_user_id = (params[:id])
-
-    #logger.info("#################{@business_user.inspect}")
-
-    #@business_company.business_user_id = params[:id]
-
-    #if @business_company.save!
-    # flash[:success] = "company added"
-    # redirect_to get_cause_to_business_business_user_path
-    #Todo: Take the B_User_Id for business company & save it
-    #Todo: redirecting to Business User select only one cause
-    #end
+    @business_company = BusinessCompany.new
   end
 
   def save_business_detail
-    #logger.info("#################{@business_company.inspect}")
     #@business_company = BusinessCompany.build(@business_company.business_user_id)
-    @business_company = BusinessCompany.create(params[:business_company])
-    #@business_company.attributes = params[:business_company]
+    #@business_company = BusinessCompany.create(params[:business_company])
+    @business_user = BusinessUser.find(params[:id])
+    @business_company = BusinessCompany.new(:business_user_id => @business_user.id)
+    #@business_company = @business_user.business_company
+    @business_company.attributes = params[:business_company]
 
     if @business_company.save!
       logger.info("#################{@business_company.inspect}")
